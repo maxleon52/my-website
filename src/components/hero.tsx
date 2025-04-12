9e3ba601e2ba09461db541aaa19b1d62b518e565
maxleon52 <maxleon522@gmail.com>
2025-04-12 15:32:29 -0300

    task: adjust animations

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,13 +4,16 @@ import IconLinkedin from "./svg/icon-linkedin";
 
 export function Hero() {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center space-y-4 border border-red-400">
+    <div className="flex h-screen w-full flex-col items-center justify-center space-y-4">
       <div id="welcome" className="">
         Opá, bem-vindo!
       </div>
 
       <div className="pt-12">
-        <h1 className="font-science-personal-use-only from-max-blue-700 to-max-blue-500 hover:text-max-blue-200 bg-gradient-to-br bg-clip-text text-9xl text-transparent transition-[1500ms]">
+        <h1
+          id="my-name"
+          className="animate-growthColor font-science-personal-use-only from-max-blue-700 to-max-blue-500 hover:!text-max-blue-200 bg-gradient-to-br bg-clip-text text-9xl text-transparent"
+        >
           Maxwell
         </h1>
       </div>
