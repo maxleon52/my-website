916445ed5c982cf06d0e8ad3e071eed8557eea80
maxleon52 <maxleon522@gmail.com>
2025-04-28 16:53:36 -0300

    task: add animatio wit scroll section about

--- src/app/(home)/about.tsx
+++ src/app/(home)/about.tsx
@@ -1,10 +1,42 @@
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(useGSAP);
+
 export function About() {
+  const container = useRef(null);
+
+  useGSAP(
+    () => {
+      gsap.from("#about-description-with-stack", {
+        opacity: 0,
+        duration: 2,
+        scrollTrigger: {
+          trigger: container.current,
+          start: "top center",
+          end: "50% center",
+          markers: true,
+        },
+      });
+    },
+    { scope: container },
+  );
+
   return (
     <section
+      ref={container}
       id="about"
       className="flex w-full items-center justify-center gap-8 pb-16"
     >
-      <div className="flex max-w-[1200px] gap-8">
+      <div
+        id="about-description-with-stack"
+        className="flex max-w-[1200px] gap-8"
+      >
         <div className="max-w-[650px]">
           <small className="font-science-personal-use-only text-max-yellow-200">
             I&apos;m
