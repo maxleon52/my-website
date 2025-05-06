076903c48b01b2abea28c64d792e94a5f89846eb
maxleon52 <maxleon522@gmail.com>
2025-05-06 20:21:45 -0300

    task: add animation section projects

--- src/app/(home)/projects.tsx
+++ src/app/(home)/projects.tsx
@@ -1,18 +1,64 @@
+"use client";
+
 import { TitleSection } from "@/components/title-section";
+import { cn } from "@/lib/utils";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useState } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(useGSAP);
 
 export function Projects() {
+  const [showAnimation, setShowAnimation] = useState(false);
+
+  useGSAP(() => {
+    gsap.from("#project-title", {
+      display: "none",
+      opacity: 0,
+      scrollTrigger: {
+        trigger: "#projects",
+        start: "top 70%",
+        end: "bottom 70%",
+      },
+    });
+
+    gsap.from(".card", {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: "#container-cards-projects",
+        start: "top 70%",
+        end: "bottom 70%",
+        onEnter: () => {
+          setShowAnimation(true);
+        },
+      },
+    });
+  });
+
   return (
     <section
       id="projects"
       className="flex w-full flex-col items-center justify-center gap-8 pb-16"
     >
-      <TitleSection>PROJECTS</TitleSection>
+      <div className="h-6 w-[110px]">
+        <TitleSection id="project-title" className="">
+          PROJECTS
+        </TitleSection>
+      </div>
 
-      <div className="text-max-blue-200 flex w-full items-start justify-center gap-2 text-sm">
+      <div
+        id="container-cards-projects"
+        className="text-max-blue-200 flex w-full items-start justify-center gap-2 text-sm"
+      >
         {[1, 2, 3, 4].map((_, index) => (
           <div
             key={index}
-            className="group card-project hover:!bg-max-blue-900 relative h-[330px] w-[230px] cursor-pointer rounded-sm p-2 transition-all duration-500 ease-in-out hover:h-[380px]"
+            className={cn(
+              "group card hover:!bg-max-blue-900 relative h-[330px] w-[230px] cursor-pointer rounded-sm p-2 transition-all duration-500 ease-in-out hover:h-[380px]",
+              showAnimation && "card-project",
+            )}
           >
             <div className="bg-max-blue-700 absolute top-2 left-2 z-10 h-0 w-[calc(100%_-_16px)] transition-all duration-500 ease-in-out group-hover:h-4/6"></div>
             <div className="flex h-3/5 w-full items-center justify-center">
