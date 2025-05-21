6a7a21a0085445ea6394a2564bdd625623e79920
maxleon52 <maxleon522@gmail.com>
2025-05-21 20:32:21 -0300

    task: adjust

--- src/app/(home)/projects.tsx
+++ src/app/(home)/projects.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Link from "next/link";
 import { useState } from "react";
 
 gsap.registerPlugin(ScrollTrigger);
@@ -55,38 +56,45 @@ export function Projects() {
         className="text-max-blue-200 flex w-full items-start justify-center gap-2 text-sm"
       >
         {projects.map((item, index) => (
-          <div
+          <Link
             key={index}
-            className={cn(
-              "group card hover:!bg-max-blue-900 relative h-[330px] w-[230px] cursor-pointer rounded-sm p-2 transition-all duration-500 ease-in-out hover:h-[380px]",
-              showAnimation && "card-project",
-            )}
+            href={
+              "https://www.linkedin.com/in/maxwell-alves-de-sousa-835036163/details/experience/?locale=en_US"
+            }
+            target="_blank"
           >
-            <div className="bg-max-blue-700 absolute top-2 left-2 z-10 flex h-0 w-[calc(100%_-_16px)] items-center justify-center overflow-clip transition-all duration-500 ease-in-out group-hover:h-4/6">
-              <img
-                src={item.imageBg.src}
-                alt={item.imageBg.alt}
-                className="h-auto w-full object-fill"
-                // width={75}
-                // height={75}
-              />
+            <div
+              className={cn(
+                "group card hover:!bg-max-blue-900 relative h-[330px] w-[230px] cursor-pointer rounded-sm p-2 transition-all duration-500 ease-in-out hover:h-[380px]",
+                showAnimation && "card-project",
+              )}
+            >
+              <div className="bg-max-blue-700 absolute top-2 left-2 z-10 flex h-0 w-[calc(100%_-_16px)] items-center justify-center overflow-clip transition-all duration-500 ease-in-out group-hover:h-4/6">
+                <img
+                  src={item.imageBg.src}
+                  alt={item.imageBg.alt}
+                  className="h-auto w-full object-fill"
+                  // width={75}
+                  // height={75}
+                />
+              </div>
+              <div className="flex h-3/5 w-full items-center justify-center">
+                <img
+                  src={item.logo.src}
+                  alt={item.logo.alt}
+                  className="h-[100px] w-[100px] object-contain"
+                  // width={75}
+                  // height={75}
+                />
+              </div>
+              <div className="flex h-2/5 w-full flex-col items-center justify-center gap-2">
+                <strong className="text-max-yellow-200 text-center">
+                  {item.name}
+                </strong>
+                <small>0{index + 1}</small>
+              </div>
             </div>
-            <div className="flex h-3/5 w-full items-center justify-center">
-              <img
-                src={item.image.src}
-                alt={item.image.alt}
-                className="h-[100px] w-[100px] object-contain"
-                // width={75}
-                // height={75}
-              />
-            </div>
-            <div className="flex h-2/5 w-full flex-col items-center justify-center gap-2">
-              <strong className="text-max-yellow-200 text-center">
-                {item.name}
-              </strong>
-              <small>0{index + 1}</small>
-            </div>
-          </div>
+          </Link>
         ))}
       </div>
     </section>
--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,16 +1,23 @@
 export const projects = [
   {
+    id: "1",
     name: "GEP COSTDRIVERS",
     position: "Frontend Developer",
     description:
+      "Developed a SAAS platform using React, NextJS, Material UI, Zustand, and other libraries. The platform provides unique tools and detailed information on prices and costs across various segments, helping managers make strategic decisions.",
+    resume:
       "Developed a SAAS platform using React, NextJS, Material UI, Zustand, and other libraries. The platform provides unique tools and detailed information on prices and costs across various segments, helping managers make strategic decisions.",
     skills: [
       "JavaScript",
       "TypeScript",
       "React",
       "NextJS",
+      "React Query",
+      "React Hook Form",
       "Material UI",
       "Tailwind CSS",
+      "Shadcnui",
+      "Zod",
       "Zustand",
       "Node.js",
       "vite",
@@ -19,21 +26,45 @@ export const projects = [
       "REST APIs",
     ],
     url: "https://www.gep.com",
-    image: {
+    logo: {
       src: "/logo-gep.svg",
       alt: "GEP COSTDRIVERS",
     },
+    images: [
+      {
+        src: "/logo-gep.svg",
+        alt: "GEP COSTDRIVERS",
+      },
+    ],
     imageBg: {
       src: "/bg-card-gep.png",
       alt: "GEP COSTDRIVERS",
     },
+    link: "",
+    github: "",
     startDate: "Nov 2023",
     endDate: "Present",
   },
   {
+    id: "2",
     name: "G&P",
     position: "Frontend Developer",
-    description:
+    description: `In the project "Bolsa do Povo", a payment management platform covering the entire state of São Paulo, Brazil, I worked as a frontend developer, playing a crucial role in the successful delivery of this comprehensive system. My main responsibilities and contributions included:
+
+                  ReactJS Development: I led the development of the platform's frontend using the ReactJS library. This involved creating reusable components, effective state management, and integrating various features to ensure an intuitive and responsive user experience.
+
+                  Adoption of Vite: I utilized the Vite build tool to optimize the application's loading time, significantly improving platform performance and efficiency.
+
+                  Styled-Components: I implemented dynamic and customized styles using the Styled-Components library, ensuring a modern and flexible approach to interface design. This allowed the platform to have a unique and attractive appearance.
+
+                  State Management with Context API: I used React's Context API to efficiently manage the application's global state. This facilitated data sharing and communication between components throughout the platform.
+
+                  Complex Integrations: I closely collaborated with the backend team to integrate payment systems, databases, and third-party services to ensure that payments were processed securely and efficiently across the state of São Paulo.
+
+                  Continuous Improvements: I remained attentive to user needs and requested enhancements, incorporating feedback and making regular updates to improve the platform over time.
+
+                  These activities were fundamental to the project's success, ensuring that the payment management platform for the state of São Paulo was robust, efficient, and effective, meeting user needs and providing an exceptional user experience.`,
+    resume:
       "Worked on the 'Bolsa do Povo' project, a payment management platform for the state of São Paulo, Brazil. Utilized ReactJS, Vite, Styled-Components, and Context API for efficient state management and complex integrations with backend systems.",
     skills: [
       "JavaScript",
@@ -47,21 +78,35 @@ export const projects = [
       "REST APIs",
     ],
     url: "https://www.gp.com.br",
-    image: {
+    logo: {
       src: "/logo-gp.svg",
       alt: "G&P",
     },
+    images: [
+      {
+        src: "/logo-gep.svg",
+        alt: "GEP COSTDRIVERS",
+      },
+    ],
     imageBg: {
       src: "/bg-card-gp.png",
       alt: "G&P",
     },
+    link: "",
+    github: "",
     startDate: "May 2022",
     endDate: "Nov 2023",
   },
   {
+    id: "3",
     name: "Hi Platform",
     position: "Frontend Developer",
-    description:
+    description: `In this project, as a frontend developer, I used technologies such as ReactJS, NextJS, TypeScript, Styled-Components, and GSAP to create a new institutional website. The goal was to highlight the company's revamped brand identity, focused on chatbot integration solutions for other businesses, facilitating online customer support. The website was designed to be responsive, meeting all usability criteria and adapting to various devices.
+
+My main activities included frontend development with ReactJS and NextJS to ensure optimized performance and SEO. TypeScript implementation enhanced code maintenance and reliability. I used Styled-Components for custom styles, and GSAP for engaging animations.
+
+Furthermore, a focus on responsiveness was crucial, ensuring the website provided an exceptional experience on different devices. I conducted usability testing to enhance user navigation and interaction and optimized the website's performance, ensuring fast loading times.`,
+    resume:
       "Developed a new institutional website for Hi Platform, focusing on chatbot integration solutions. Utilized ReactJS, NextJS, TypeScript, Styled-Components, and GSAP for animations. Ensured responsive design and optimized performance for various devices.",
     skills: [
       "JavaScript",
@@ -76,21 +121,41 @@ export const projects = [
       "REST APIs",
     ],
     url: "https://www.hiplatform.com.br",
-    image: {
+    logo: {
       src: "/logo-hi.svg",
       alt: "Hi Platform",
     },
+    images: [
+      {
+        src: "/logo-gep.svg",
+        alt: "GEP COSTDRIVERS",
+      },
+    ],
     imageBg: {
       src: "/bg-card-hi.png",
       alt: "Hi Platform",
     },
+    link: "",
+    github: "",
     startDate: "Apr 2021",
     endDate: "May 2022",
   },
   {
+    id: "4",
     name: "Flimed",
     position: "Software Engineer",
-    description:
+    description: `Management and Control Platform for Medical Activities:
+
+This platform encompasses critical functionalities such as appointment scheduling, telemedicine, online medical records, patient management, clinic administration, healthcare professional tracking, exam management, financial oversight, and accounting. My contribution involved implementing crucial features to ensure the efficiency and security of these essential medical operations.
+
+FLIMED Website Integrated with the Platform:
+
+The website provided patients with easy access to information about doctors, clinics, appointment scheduling, teleconsultations, and other health-related resources. This integration ensured a seamless experience for patients, enabling them to conveniently access and benefit from all of FLIMED's services.
+
+Exclusive FLIMED Administrative Dashboard:
+
+Designed for internal company use, this dashboard was created to provide comprehensive control and insights to FLIMED's clients. Its functionalities included a dashboard, financial tools, accounting resources, information about FLIMED's membership plans, data relevant to marketing strategies, detailed administrative reports, and robust user management capabilities. This dashboard played a fundamental role in optimizing FLIMED's internal operations.`,
+    resume:
       "Contributed to the development of a management and control platform for medical activities, including appointment scheduling, telemedicine, and patient management. Utilized ReactJS, NodeJS, Redux, Styled-Components, and NextJS.",
     skills: [
       "JavaScript",
@@ -105,14 +170,22 @@ export const projects = [
       "REST APIs",
     ],
     url: "https://www.flimed.com.br",
-    image: {
+    logo: {
       src: "/logo-flimed.svg",
       alt: "Flimed",
     },
+    images: [
+      {
+        src: "/logo-gep.svg",
+        alt: "GEP COSTDRIVERS",
+      },
+    ],
     imageBg: {
       src: "/bg-card-flimed.png",
       alt: "Flimed",
     },
+    link: "",
+    github: "",
     startDate: "Aug 2020",
     endDate: "Apr 2021",
   },
